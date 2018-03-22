2febaaa2d25a9a54495e577832501dab5e2ecc1a
Pranav Nutalapati <pranavnutalapati@gmail.com>
2018-03-22 20:39:13 +0530

    ???

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -8,7 +8,7 @@ const managementController = require("../controllers/managementController")
 module.exports = app => {
 	app.use(express.static(path.resolve(__dirname, "../client/static")))
 
-	app.use('/images', express.static(studentElectionsSenior.imagesDir))
+	app.use('/images', express.static(studentElectionsJunior.imagesDir))
 
 	app.post('/api/check', clientController.check)
 	app.get('/api/fetch', clientController.fetch)
